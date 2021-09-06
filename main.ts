ae71a426c03df9806f68cbbb9411cf298e52fb65
Zayas26 <90221820+Zayas26@users.noreply.github.com>
2021-09-06 16:09:40 -0600

    Delete main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-function imprimirEtiqueta (etiqueta: {label:string}){
-    console.log(etiqueta.label);
-}
-
-let miEtiqueta = { numero: 10, label: "esta es mi etiqueta"};
-imprimirEtiqueta(miEtiqueta);
